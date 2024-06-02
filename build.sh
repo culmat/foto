9f761d785b599ef4aa0469e8ef85d95dc96beea5
Matthias Cullmann <matthias.cullmann@gmail.com>
2024-06-02 21:27:42 +0200

    using docker

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
- thumbsup --config thumbsUp_config.json --input ./src --output .
+# thumbsup --config thumbsUp_config.json --input ./src --output .
+docker run -t  -v "$(pwd):/work" -u $(id -u):$(id -g)  ghcr.io/thumbsup/thumbsup thumbsup --input /work/src --output /work --config /work/thumbsUp_config.json
