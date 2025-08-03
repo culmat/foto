6f56d6905b5659e5eb20db97678cf9922c7a9a57
Matthias Cullmann <matthias.cullmann@gmail.com>
2025-08-03 12:39:10 +0200

    improved scripts

---
+++ review.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DIR="${1:-/home/matthi/Pictures/sigal/src/WIP/}"
+feh -.Fn -A "shotwell -f %F &" "$DIR"
+
--- select.sh
+++ select.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-feh -.Fn -A "mkdir -p ~/Pictures/sigal/src/WIP && cp %F ~/Pictures/sigal/src/WIP/%N" ~/Pictures/s10/Camera/
+SRC_DIR="${1:-/home/matthi/Pictures/s10/Camera/}"
+DEST_DIR="${2:-/home/matthi/Pictures/sigal/src/WIP/}"
+
+feh --edit -.Fn -A ";mkdir -p \"$DEST_DIR\" && cp %F \"$DEST_DIR/%n\"" "$SRC_DIR"
