56c96374e47acca0e6f1c524d8a8d42424531063
Matthias Cullmann <matthias.cullmann@gmail.com>
2025-09-10 18:08:14 +0200

    DOC

--- select.sh
+++ select.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-SRC_DIR="${1:-/home/matthi/Pictures/s10/Camera/}"
-DEST_DIR="${2:-/home/matthi/Pictures/sigal/src/WIP/}"
-
-feh --edit -.Fn -A ";mkdir -p \"$DEST_DIR\" && cp %F \"$DEST_DIR/%n\"" "$SRC_DIR"
+DEST_DIR="${1:-/home/matthi/Pictures/sigal/src/WIP/}"
+SRC_DIR="${2:-/home/matthi/Pictures/s10/Camera/}"
+# press 0 to copy to DEST_DIR
+# press 1 to copy to clipboard
+# see https://man.finalrewind.org/1/feh/
+feh --edit -.Fn -A ";mkdir -p \"$DEST_DIR\" && cp %F \"$DEST_DIR/%n\"" --action1 'xclip -selection clipboard -t image/%t -i %F' "$SRC_DIR"
