086ae6e880e883e51ea99c2bd8a538280aafcb50
Matthias Cullmann <matthias.cullmann@gmail.com>
2025-10-01 19:56:36 +0200

    add wallpaper action

--- select.sh
+++ select.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 DEST_DIR="${1:-/home/matthi/Pictures/sigal/src/WIP/}"
 SRC_DIR="${2:-/home/matthi/Pictures/s10/Camera/}"
+WALLPAPER_DIR="${3:-/home/matthi/Pictures/Wallpapers}"
 # press 0 to copy to DEST_DIR
 # press 1 to copy to clipboard
+# press 2 to copy to WALLPAPER_DIR
 # see https://man.finalrewind.org/1/feh/
-feh --edit -.Fn -A ";mkdir -p \"$DEST_DIR\" && cp %F \"$DEST_DIR/%n\"" --action1 'xclip -selection clipboard -t image/%t -i %F' "$SRC_DIR"
+feh --edit -.Fn -A ";mkdir -p \"$DEST_DIR\" && cp %F \"$DEST_DIR/%n\"" --action1 'xclip -selection clipboard -t image/%t -i %F' --action2 ";mkdir -p \"$WALLPAPER_DIR\" && cp %F \"$WALLPAPER_DIR/%n\"" "$SRC_DIR"
