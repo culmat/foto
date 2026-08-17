27e45d1917af2698b53128011733a50778616cf1
Matthias Cullmann <matthias.cullmann@gmail.com>
2026-08-17 18:46:08 +0200

    update serve.sh to display LAN address and bind to all interfaces

--- serve.sh
+++ serve.sh
@@ -1,1 +1,2 @@
-python3 -m http.server 8000 --bind 127.0.0.1
+echo "LAN: http://$(ipconfig getifaddr en0 2>/dev/null || ipconfig getifaddr en1 2>/dev/null || echo localhost):8000"
+python3 -m http.server 8000 --bind 0.0.0.0
